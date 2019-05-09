22c822bfcd1c8e0ab8c5b27b5ef24773d82ed0f9
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-10 00:53:19 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,7 @@ module.exports.run = async (blaki, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Usunięto ${args[0]} wiadomości.`).then(msg => msg.delete(2000));
+    message.channel.send(`Usunięto ${args[0]} wiadomości!`).then(msg => msg.delete(2000));
 });
 }
 
