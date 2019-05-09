1a0f9eaddd03cd9e995225e8476af345e01ec6a6
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-10 00:51:32 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,14 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (blaki, message, args) => {
 
-    if(!message.member.hasPermission("MANAGE_MESSAGE")) return message.reply("oops");
-    if(!args[0]) return message.channel.send("oops");
-    
-    message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`Wyczyszczono ${args[0]} wiadomości.`).then(msg => msg.delete(2000));
-    });
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+  if(!args[0]) return message.channel.send("no");
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+});
+
 }
 
 module.exports.help = {
-    name: "clear"
+  name: "clear"
 }
