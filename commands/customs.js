338bc34b5bbefdb541ca37740133043234e5579d
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-10 13:33:25 +0200

    Add files via upload

---
+++ commands/customs.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (blaki, message, args) => {
+
+    let Szefuncio = message.guild.roles.find("name", "SZEFUNCIO 🎓");
+    let pass = args[1];
+    let mode = args[0];
+
+    if(!message.member.roles.has(Szefuncio.id)) return message.reply("oops");
+    else {
+
+        let customEmbed = new Discord.RichEmbed()
+        .setColor("#FF0000")
+        .setTitle("__**POWIADOMIENIE O NOWEJ GRZE**__")
+        .addField("HASŁO", `**${pass}**`, true)
+        .addField("TRYB GRY", `**${mode}**`, true)
+        .setTimestamp()
+        .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/g10ycEV.png");
+        
+        message.channel.send(customEmbed).then(function (message) {
+            message.react("✅")
+        })
+    }
+}
+
+module.exports.help = {
+    name: "custom"
+}
