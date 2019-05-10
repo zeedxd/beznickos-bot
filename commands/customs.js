e5773f26015eb6440c4039832f31cba0df20f1fc
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-10 14:37:17 +0200

    Update customs.js

--- commands/customs.js
+++ commands/customs.js
@@ -3,28 +3,28 @@ const Discord = require("discord.js");
 module.exports.run = async (blaki, message, args) => {
 
     let Szefuncio = message.guild.roles.find("name", "SZEFUNCIO 🎓");
-    let pass = args[1];
-    let mode = args[0];
+
+    let pass = (args[0]);
+    let mode = args.join(" ").slice(7);
 
     if(!message.member.roles.has(Szefuncio.id)) return message.reply("oops");
-    if(!args[0]) return message.channel.send("❌ Wprowadź prawidłowe wartości, b!custom tryb hasło ❌");
-    else {
-        message.delete();
-        let customEmbed = new Discord.RichEmbed()
-        .setColor("#FF0000")
-        .setTitle("__**POWIADOMIENIE O NOWEJ GRZE**__")
-        .addField("HASŁO", `**${pass}**`, true)
-        .addField("TRYB GRY", `**${mode}**`, true)
-        .setTimestamp()
-        .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/g10ycEV.png");
-        
-        message.channel.send(customEmbed).then(function (message) {
-            message.react("✅")
-        })
-    }
+    if(!args[0]) return message.channel.send("❌ Wprowadź prawidłowe wartości, b!ct hasło tryb ❌").then(() =>
+    {
+        message.channel.send("❌ Hasło musi posiadać max 7 znaków! ❌");
+    })
+    let customEmbed = new Discord.RichEmbed()
+    .setColor("#FF0000")
+    .setTitle("__**POWIADOMIENIE O NOWEJ GRZE**__")
+    .addField("HASŁO", `**${pass}**`)
+    .addField("TRYB GRY", `**${mode}**`)
+    .setTimestamp(message.createdAt)
+    .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/g10ycEV.png");
+    
+    message.channel.send(customEmbed).then(function (message) {
+        message.react("✅")
+    })
 }
 
 module.exports.help = {
-    name: "custom",
-    name: "c"
+    name: "ct"
 }
