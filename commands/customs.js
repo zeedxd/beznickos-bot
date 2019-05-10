bb7dace73ca39397c32a670b5978b11b2d24be06
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-10 14:53:01 +0200

    Update customs.js

--- commands/customs.js
+++ commands/customs.js
@@ -8,9 +8,9 @@ module.exports.run = async (blaki, message, args) => {
     let mode = args.join(" ").slice(7);
 
     if(!message.member.roles.has(Szefuncio.id)) return message.reply("oops");
-    if(!args[0]) return message.channel.send("❌ Wprowadź prawidłowe wartości, **b!ct hasło tryb** ❌").then(() =>
+    if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **b!ct hasło tryb**_ ❌").then(() =>
     {
-        message.channel.send("❌ _Hasło musi posiadać dokładnie 7 znaków!_ ❌");
+        message.channel.send("❌ _**Hasło musi posiadać dokładnie 7 znaków!**_ ❌");
     })
     message.delete();
     let customEmbed = new Discord.RichEmbed()
